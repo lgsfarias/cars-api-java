50ab12ac03b41b36083411abc0d997089fb24944
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-18 14:30:08 -0300

    feat: print cars information when request succeeds

--- src/main/java/com/carsapi/api/controller/CarController.java
+++ src/main/java/com/carsapi/api/controller/CarController.java
@@ -1,15 +1,23 @@
 package com.carsapi.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carsapi.api.dto.CarsDTO;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
 
   @PostMapping
-  public String create() {
+  public String create(@RequestBody CarsDTO req) {
+    System.out.println("modelo: " + req.modelo() +
+        "\nfabricante: " + req.fabricante() +
+        "\ndataFabricacao: " + req.dataFabricacao() +
+        "\nvalor: " + req.valor() +
+        "\nanoModelo: " + req.anoModelo());
     return "Car created";
   }
 }
