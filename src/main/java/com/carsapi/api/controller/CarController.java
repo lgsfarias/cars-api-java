cffdbdef29eb6156bf91f18fa87cf6dc9ea413d8
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-18 15:11:45 -0300

    feat: information is persisted when car is saved

--- src/main/java/com/carsapi/api/controller/CarController.java
+++ src/main/java/com/carsapi/api/controller/CarController.java
@@ -1,15 +1,21 @@
 package com.carsapi.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carsapi.api.dto.CarsDTO;
+import com.carsapi.api.model.Car;
+import com.carsapi.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
+
+  @Autowired
+  private CarRepository repository;
 
   @PostMapping
   public String create(@RequestBody CarsDTO req) {
@@ -18,6 +24,8 @@ public class CarController {
         "\ndataFabricacao: " + req.dataFabricacao() +
         "\nvalor: " + req.valor() +
         "\nanoModelo: " + req.anoModelo());
+
+    repository.save(new Car(req));
     return "Car created";
   }
 }
