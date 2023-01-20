230666d0fc5e74b0176d65133991d21ae7b14bbb
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-20 10:53:20 -0300

    feat: create delete route

--- src/main/java/com/carsapi/api/controller/CarController.java
+++ src/main/java/com/carsapi/api/controller/CarController.java
@@ -3,7 +3,9 @@ package com.carsapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.carsapi.api.dto.CarsDTO;
 import com.carsapi.api.model.Car;
 import com.carsapi.api.repository.CarRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -26,7 +30,7 @@ public class CarController {
   }
 
   @PostMapping
-  public String create(@RequestBody CarsDTO req) {
+  public String create(@RequestBody @Valid CarsDTO req) {
     System.out.println("modelo: " + req.modelo() +
         "\nfabricante: " + req.fabricante() +
         "\ndataFabricacao: " + req.dataFabricacao() +
@@ -35,5 +39,11 @@ public class CarController {
 
     repository.save(new Car(req));
     return "Car created";
+  }
+
+  @DeleteMapping("/{id}")
+  public String delete(@PathVariable Long id) {
+    repository.deleteById(id);
+    return "Car deleted";
   }
 }
--- src/main/java/com/carsapi/api/dto/CarsDTO.java
+++ src/main/java/com/carsapi/api/dto/CarsDTO.java
@@ -1,6 +1,7 @@
 package com.carsapi.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record CarsDTO(
@@ -11,8 +12,8 @@ public record CarsDTO(
 
     @NotBlank(message = "O campo data de fabricação é obrigatório") @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}$", message = "O campo data de fabricação deve estar no formato dd/MM/yyyy") String dataFabricacao,
 
-    @NotBlank(message = "O campo valor é obrigatório") Double valor,
+    @NotNull(message = "O campo valor é obrigatório") Double valor,
 
-    @NotBlank(message = "O campo ano do modelo é obrigatório") Integer anoModelo) {
+    @NotNull(message = "O campo ano do modelo é obrigatório") Integer anoModelo) {
 
 }
