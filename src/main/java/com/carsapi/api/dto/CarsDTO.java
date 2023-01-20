8ff7f11da121f77b88a34a8a3598701fe22b8abe
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-20 10:40:22 -0300

    feat: add validations to car DTO

--- src/main/java/com/carsapi/api/dto/CarsDTO.java
+++ src/main/java/com/carsapi/api/dto/CarsDTO.java
@@ -1,5 +1,18 @@
 package com.carsapi.api.dto;
 
-public record CarsDTO(String modelo, String fabricante, String dataFabricacao, Double valor, Integer anoModelo) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record CarsDTO(
+
+    @NotBlank(message = "O campo modelo é obrigatório") String modelo,
+
+    @NotBlank(message = "O campo fabricante é obrigatório") String fabricante,
+
+    @NotBlank(message = "O campo data de fabricação é obrigatório") @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}$", message = "O campo data de fabricação deve estar no formato dd/MM/yyyy") String dataFabricacao,
+
+    @NotBlank(message = "O campo valor é obrigatório") Double valor,
+
+    @NotBlank(message = "O campo ano do modelo é obrigatório") Integer anoModelo) {
 
 }
